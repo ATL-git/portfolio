52853c90852054e263e3db2d0700814267f96428
xoxoATLxoxo <a.maureau13400@gmailcom>
2024-04-03 17:10:51 +0200

    first commit

---
+++ assets/javascript/script.js
@@ -0,0 +1,39 @@
+function previous(){
+    const widthSlider = document.querySelector(".slider").offsetWidth;
+    const sliderContent = document.querySelector('.slider_content');
+    sliderContent.scrollLeft -= widthSlider;
+    const scrollLeft = sliderContent.scrollLeft;
+    const itemSlider = sliderContent.querySelectorAll('.slider_item');
+   
+    if (scrollLeft <= 0) {
+        sliderContent.scrollLeft = widthSlider * (itemSlider.length - 1);
+    }
+}
+
+
+function next(){
+    const widthSlider = document.querySelector(".slider").offsetWidth ;
+    const sliderContent = document.querySelector('.slider_content')
+    sliderContent.scrollLeft += widthSlider ;
+    const scrollLeft = sliderContent.scrollLeft
+   const itemSlider = sliderContent.querySelectorAll('.slider_item')
+   
+   if (scrollLeft == widthSlider * (itemSlider.length -1 )) {
+    sliderContent.scrollLeft = 0;
+   }
+}
+
+
+function openModal(src){
+    document.querySelector('.overlay').style.display ='block' ;
+    document.querySelector('.modal').classList.add('modalOpen')
+   
+    document.querySelector('#ifram').src = src
+   
+}
+
+function closeModal(){
+    document.querySelector('.overlay').style.display ='none' ;
+    document.querySelector('.modal').classList.remove('modalOpen')
+    
+}
